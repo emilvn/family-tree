1c6c63f43dcb1a6e10b53abc66d78784a16cea7b
emilvn <emilvnielsen@hotmail.com>
2024-02-23 15:55:19 +0100

    init footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "inact-green": "#304642",
+        "inact-violet": "#E7E2EF"
+      }
+    },
   },
   plugins: [
   ],
