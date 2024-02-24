558f0dbd1f0c6b5c102e98dfd07c46a3c191bccb
emilvn <emilvnielsen@hotmail.com>
2024-02-24 23:02:24 +0100

    refactored nav to layout.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FamilyTree from "./containers/FamilyTree.tsx";
 import Statistics from "./containers/Statistics.tsx";
 import { family, henning } from "../data/family.ts";
-import { useEffect, useState } from "react";
-import { CiMenuBurger } from "react-icons/ci";
-import { IoCloseOutline } from "react-icons/io5";
-import { GiFamilyTree } from "react-icons/gi";
-import { INavBarProps } from "./types/props.types.ts";
 
 function App() {
 	return (
 		<BrowserRouter>
-			<Nav />
 			<Routes>
 				<Route
 					path={"/"}
@@ -23,116 +17,6 @@ function App() {
 				/>
 			</Routes>
 		</BrowserRouter>
-	);
-}
-
-function Nav() {
-	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-	const [location, setLocation] = useState<"family-tree" | "statistics">(
-		"family-tree"
-	);
-
-	useEffect(() => {
-		if (window.location.pathname === "/") {
-			setLocation("family-tree");
-		} else if (window.location.pathname === "/statistics") {
-			setLocation("statistics");
-		}
-	}, []);
-
-	useEffect(() => {
-		function handleResize() {
-			setWindowWidth(window.innerWidth);
-		}
-
-		window.addEventListener("resize", handleResize);
-		return () => window.removeEventListener("resize", handleResize);
-	}, []);
-
-	return (
-		<div className="p-4 fixed w-full flex gap-4 items-center justify-between select-none z-20">
-			{windowWidth > 768 && (
-				<>
-					<h1 className="text-5xl font-bold text-inact-green flex items-center">
-						Henning
-						<GiFamilyTree />
-					</h1>
-					<NavBar
-						location={location}
-						setLocation={setLocation}
-					/>
-				</>
-			)}
-			{windowWidth <= 768 && (
-				<>
-					<h1 className="text-5xl font-bold text-inact-green">
-						<GiFamilyTree />
-					</h1>
-					<NavButton
-						location={location}
-						setLocation={setLocation}
-					/>
-				</>
-			)}
-		</div>
-	);
-}
-
-function NavButton({ location, setLocation }: INavBarProps) {
-	const [isOpen, setIsOpen] = useState(false);
-	return (
-		<div>
-			{!isOpen && (
-				<CiMenuBurger
-					onClick={() => setIsOpen(!isOpen)}
-					className="text-4xl hover:text-orange-600 text-inact-green cursor-pointer"
-				/>
-			)}
-			{isOpen && (
-				<>
-					<div className="flex gap-4 text-slate-600 text-xl max-sm:text-sm">
-						<NavBar
-							location={location}
-							setLocation={setLocation}
-							onClick={() => {
-								setIsOpen(!isOpen);
-							}}
-						/>
-						<IoCloseOutline
-							onClick={() => setIsOpen(!isOpen)}
-							className="text-4xl hover:text-orange-600 text-inact-green cursor-pointer"
-						/>
-					</div>
-				</>
-			)}
-		</div>
-	);
-}
-
-function NavBar({ location, onClick, setLocation }: INavBarProps) {
-	return (
-		<nav className="flex gap-4 text-slate-600 text-xl">
-			<Link
-				className={`${location === "family-tree" ? "text-orange-600" : "hover:text-orange-600"}  transition-colors`}
-				to={"/"}
-				onClick={() => {
-					setLocation("family-tree");
-					if (onClick) onClick();
-				}}
-			>
-				Family-tree
-			</Link>
-			<Link
-				className={`${location === "statistics" ? "text-orange-600" : "hover:text-orange-600"} transition-colors`}
-				to={"/statistics"}
-				onClick={() => {
-					setLocation("statistics");
-					if (onClick) onClick();
-				}}
-			>
-				Statistics
-			</Link>
-		</nav>
 	);
 }
 
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,12 +1,128 @@
-import { PropsWithChildren } from "react";
+import { PropsWithChildren, useEffect, useState } from "react";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { GiFamilyTree } from "react-icons/gi";
+import { INavBarProps } from "../types/props.types.ts";
+import { CiMenuBurger } from "react-icons/ci";
+import { IoCloseOutline } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 function PageLayout({ children }: PropsWithChildren) {
 	return (
 		<div className="bg-inact-violet w-full min-h-screen flex flex-col justify-between">
+			<Nav />
 			<main className="pt-20 pb-10 px-10">{children}</main>
 			<Footer />
 		</div>
+	);
+}
+
+function Nav() {
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+	const [location, setLocation] = useState<"family-tree" | "statistics">(
+		"family-tree"
+	);
+
+	useEffect(() => {
+		if (window.location.pathname === "/") {
+			setLocation("family-tree");
+		} else if (window.location.pathname === "/statistics") {
+			setLocation("statistics");
+		}
+	}, []);
+
+	useEffect(() => {
+		function handleResize() {
+			setWindowWidth(window.innerWidth);
+		}
+
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
+	}, []);
+
+	return (
+		<div className="p-4 fixed w-full flex gap-4 items-center justify-between select-none z-20">
+			{windowWidth > 768 && (
+				<>
+					<h1 className="text-5xl font-bold text-inact-green flex items-center">
+						Henning
+						<GiFamilyTree />
+					</h1>
+					<NavBar
+						location={location}
+						setLocation={setLocation}
+					/>
+				</>
+			)}
+			{windowWidth <= 768 && (
+				<>
+					<h1 className="text-5xl font-bold text-inact-green">
+						<GiFamilyTree />
+					</h1>
+					<NavButton
+						location={location}
+						setLocation={setLocation}
+					/>
+				</>
+			)}
+		</div>
+	);
+}
+
+function NavButton({ location, setLocation }: INavBarProps) {
+	const [isOpen, setIsOpen] = useState(false);
+	return (
+		<div>
+			{!isOpen && (
+				<CiMenuBurger
+					onClick={() => setIsOpen(!isOpen)}
+					className="text-4xl hover:text-orange-600 text-inact-green cursor-pointer"
+				/>
+			)}
+			{isOpen && (
+				<>
+					<div className="flex gap-4 text-slate-600 text-xl max-sm:text-sm">
+						<NavBar
+							location={location}
+							setLocation={setLocation}
+							onClick={() => {
+								setIsOpen(!isOpen);
+							}}
+						/>
+						<IoCloseOutline
+							onClick={() => setIsOpen(!isOpen)}
+							className="text-4xl hover:text-orange-600 text-inact-green cursor-pointer"
+						/>
+					</div>
+				</>
+			)}
+		</div>
+	);
+}
+
+function NavBar({ location, onClick, setLocation }: INavBarProps) {
+	return (
+		<nav className="flex gap-4 text-slate-600 text-xl">
+			<Link
+				className={`${location === "family-tree" ? "text-orange-600" : "hover:text-orange-600"}  transition-colors`}
+				to={"/"}
+				onClick={() => {
+					setLocation("family-tree");
+					if (onClick) onClick();
+				}}
+			>
+				Family-tree
+			</Link>
+			<Link
+				className={`${location === "statistics" ? "text-orange-600" : "hover:text-orange-600"} transition-colors`}
+				to={"/statistics"}
+				onClick={() => {
+					setLocation("statistics");
+					if (onClick) onClick();
+				}}
+			>
+				Statistics
+			</Link>
+		</nav>
 	);
 }
 
